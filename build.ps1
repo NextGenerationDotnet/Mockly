fb57b4c6a6badf4ce527de8e5c63b13c752ff0fb
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-08-16 12:17:44 +0100

    Add initial project structure (#1)

---
+++ build.ps1
@@ -0,0 +1,47 @@
+#! /usr/bin/env pwsh
+#Requires -Version 7.0
+#Requires -PSEdition Core
+
+param (
+    [string] $Configuration = "Release",
+    [string] $OutputPath = (Join-Path $PSScriptRoot "artifacts"),
+    [switch] $SkipTests = $false
+)
+
+$buildProjectPaths = @(
+)
+
+$testProjectPaths = @(
+    "tests/Mockly.Functional.Tests/Mockly.Functional.Tests.csproj"
+)
+
+$packageProjectPaths = @(
+    "src/Mockly/Mockly.csproj"
+)
+
+
+foreach ($buildProjectPath in $buildProjectPaths) {
+    dotnet build $buildProjectPath --configuration $Configuration
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "dotnet build failed with exit code $LASTEXITCODE"
+    }
+}
+
+if (-not $SkipTests) {
+    foreach ($testProjectPath in $testProjectPaths) {
+        dotnet test $testProjectPath --configuration $Configuration
+    
+        if ($LASTEXITCODE -ne 0) {
+            throw "dotnet test failed with exit code $LASTEXITCODE"
+        }
+    }
+}
+
+foreach ($packageProjectPath in $packageProjectPaths) {
+    dotnet pack $packageProjectPath --configuration $Configuration
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "dotnet pack failed with exit code $LASTEXITCODE"
+    }
+}
---
+++ src/Mockly.Generator/MocklyGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Mockly.Generator;
+
+[Generator]
+public class MocklyGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
